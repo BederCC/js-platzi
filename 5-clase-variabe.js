20b931289f018994cc5e003249b84f414f827a02
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-11-24 23:24:25 -0500

    Add files via upload

---
+++ 5-clase-variabe.js
@@ -0,0 +1,12 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy);
+
+//no usar variables ambiguas
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+//Lo permitido
+let primerTasteoDeAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '1234567890'
