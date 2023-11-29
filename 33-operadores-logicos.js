1e7386b18f8dc4dd066253a8e9c7a7c3c66104c7
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-11-28 21:35:31 -0500

    Add files via upload

---
+++ 33-operadores-logicos.js
@@ -0,0 +1,15 @@
+/*
+    Operadores Lógicos
+*/
+
+// && (AND): Devuelve true si ambos operandos son true.
+// || (OR): Devuelve true si al menos uno de los operandos es true.
+// ! (NOT): Devuelve true si el operando es false.
+
+const a=10;
+const b=20;
+const c="10"
+
+a==b && a===c; //false
+a!=b || a===c; //true
+!(a === c); //true
