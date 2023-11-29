15836c29d9cd99df0d3c104ca439e182a6da8bdc
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-11-29 02:23:53 -0500

    Add files via upload

---
+++ 36-clase-condicional-ternario.js
@@ -0,0 +1,16 @@
+// let mensaje
+// const edad = 18;
+// if (edad >= 18) {
+//     mensaje = 'Eres mayor de edad';
+// } else {    
+//     mensaje = 'Eres menor de edad';
+// }
+// console.log(mensaje);
+
+// Estructura condicional ternaria
+// condicion ? true : false;
+
+const edad = 15;
+
+const mensaje = edad >= 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
+console.log(mensaje);
