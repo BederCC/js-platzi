00f8ef328fc39f6234522f6ae1a9c61d41275bbb
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-12-03 21:47:00 -0500

    Add files via upload

---
+++ 40-clase-loop-for-of.js
@@ -0,0 +1,15 @@
+/*
+
+for of array, string [algo]
+
+for (variable of iterable) {
+  // código a ejecutar
+}
+
+*/
+
+let canasta = ['manzana', 'pera', 'naranja', 'uva'];
+
+for(fruta of canasta) {
+    console.log(fruta);
+}
---
+++ 41-clase-loop-for-in.js
@@ -0,0 +1,30 @@
+/*
+
+for in --> objetos
+
+propiedades = valor
+
+array, string
+
+item
+
+for (variable){
+    // código a ejecutar
+}
+
+*/
+
+const listaDeCompras = {
+    'manzanas': 1,
+    'peras': 2,
+    'naranjas': 3,
+    'uvas': 4
+};
+
+for(fruta in listaDeCompras) {
+    console.log(fruta);
+}
+
+for(fruta in listaDeCompras) {
+    console.log(`${fruta}: ${listaDeCompras[fruta]}`);
+}
